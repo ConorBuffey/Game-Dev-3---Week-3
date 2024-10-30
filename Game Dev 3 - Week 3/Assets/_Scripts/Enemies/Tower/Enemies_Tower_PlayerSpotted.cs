51a06b7b61743c6e49d6c55ec50b105ffc12fe9f
ConorBuffey <c.buffey3244@student.leedsbeckett.ac.uk>
2024-10-30 20:25:43 +0000

    The sound works

--- Game Dev 3 - Week 3/Assets/_Scripts/Enemies/Tower/Enemies_Tower_PlayerSpotted.cs
+++ Game Dev 3 - Week 3/Assets/_Scripts/Enemies/Tower/Enemies_Tower_PlayerSpotted.cs
@@ -1,17 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
+using GameDevWithMarco.ObserverPattern;
 using UnityEngine;
 
 namespace GameDevWithMarco.Enemies
 {
     public class Enemies_Tower_PlayerSpotted : MonoBehaviour
     {
+        [SerializeField] GameEvent playerSpotted;
+
         private void OnTriggerEnter2D(Collider2D collision)
         {
             //If the player enters the collider, the spotted event is raised!
             if (collision.gameObject.tag == "Player")
             {
                 //Do something
+                playerSpotted.Raise();
             }
         }
     }
--- Game Dev 3 - Week 3/Assets/_Scripts/ObserverPattern/GameEvent.cs
+++ Game Dev 3 - Week 3/Assets/_Scripts/ObserverPattern/GameEvent.cs
@@ -39,4 +39,3 @@ namespace GameDevWithMarco.ObserverPattern
         }
     }
 }
-
